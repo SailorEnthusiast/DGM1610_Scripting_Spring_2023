317612279c69828fcf92310c8e2af66a78d77738
SailorEnthusiast <122577256+SailorEnthusiast@users.noreply.github.com>
2023-01-22 17:45:30 -0700

    Completed Variable Assignment

---
+++ Variables_Assignment.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace VariablesAssignment
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int age = 118;
+      int friends = 4;
+      double height = 6.6273d;
+      float weight = 94.8f;
+      char initFirst = 'M';
+      char initLast = 'D';
+      string nameFirst = "Malleus";
+      string nameLast = "Draconia";
+      bool royalty = true;
+      bool human = false;
+    
+      Console.WriteLine(age);
+      Console.WriteLine(friends);
+      Console.WriteLine(height);
+      Console.WriteLine(weight);
+      Console.WriteLine(initFirst);
+      Console.WriteLine(initLast);
+      Console.WriteLine(nameFirst);
+      Console.WriteLine(nameLast);
+      Console.WriteLine(royalty);
+      Console.WriteLine(human);
+    }
+  }
+}
