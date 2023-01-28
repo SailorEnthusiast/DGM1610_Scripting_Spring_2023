8f8c2d22f15955da4175553a003057fc55a3890d
Madison Hayward <18mhayward@gmail.com>
2023-01-28 14:57:14 -0700

    Added Variable and Move scripts

---
+++ First Prototype/Assets/Scripts/Variables.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // [Access Modifier] [Data Type/Type] [Identifier/Name] [Access Oporator =] [Value]
+
+    public string herosName = "Batman";
+
+    private int herosAge = 30;
+
+    public float heroHeight = 6.2f;
+    
+    public char heroLetter = 'B';
+
+    private double heroNetWorth = 100.45d;
+
+    public bool isHeroInvincible = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
