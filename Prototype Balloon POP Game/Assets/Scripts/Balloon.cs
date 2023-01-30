bfe190bc5f8bf4660061a0b4cefb6928fb7e8c37
Madison Hayward <18mhayward@gmail.com>
2023-01-29 18:21:51 -0700

    Adjusted Balloon POP script

--- Prototype Balloon POP Game/Assets/Scripts/Balloon.cs
+++ Prototype Balloon POP Game/Assets/Scripts/Balloon.cs
@@ -15,7 +15,7 @@ public class Balloon : MonoBehaviour
 
     void onMouseDown() 
     {
-        clickToPop -= 1; // Reduce clicks by one
+        clickToPop --; // Reduce clicks by one
         // Inflate balloon
         transform.localScale += Vector3.one * scaleToIncrease;
         // Check to see if click to pop has reached zero. Check to see if the balloon pops
